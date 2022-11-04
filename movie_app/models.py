e82cdcb955c36e4b851b5694b6ec98b2162dc2a7
Demilorakai <104967684+Demilorakai@users.noreply.github.com>
2022-11-04 12:50:07 +0300

    second commit

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Director(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.TextField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -14,13 +12,53 @@ class Movie(models.Model):
     title = models.TextField(max_length=100)
     description = models.CharField(max_length=250)
     duration = models.IntegerField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
 
+    @property
+    def category_name(self):
+        try:
+            return self.director.name
+        except:
+            return 'no category'
+
+    @property
+    def filter_reviews(self):
+        #return self.reviews.filter(stars__gt=3)
+     return self.reviews.filter(stars__in=[4, 5])
+
+    @property
+    def rating(self):
+        reviews = self.filter_reviews
+        count = reviews.count()
+        sum_ = 0
+        for i in reviews:
+            sum_+= i.stars
+        try:
+          return sum_ / count
+        except ZeroDivisionError:
+            return 0
+
+
+STARS = (
+    (1, '*'),
+    (2, '**'),
+    (3, '***'),
+    (4, '****'),
+    (5, '*****')
+)
+
 
 class Review(models.Model):
     text = models.CharField(max_length=280)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    stars = models.IntegerField(default=5, choices=STARS)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return self.text
+
+    def stars_str(self):
+        return self.stars * '*'
 
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -17,4 +17,21 @@ class MovieSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = 'id text movie'.split()
+        fields = 'id text stars stars_str'.split()
+
+
+class MoviSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+    filter_reviews = ReviewSerializer(many=True)
+    category = DirectorSerializer()
+    category_name_copy = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = 'id title reviews filter_reviews rating category category_name category_name_copy'.split()
+
+    def get_category_name_copy(self, movie):
+        try:
+           return movie.category.name
+        except:
+            return ''
